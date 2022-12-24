ff59cae952464f80506bd817eceb3598f9887271
enginearn <enginyaaaaan@gmail.com>
2022-12-24 13:30:58 +0900

    Added

--- section_005/assignmentSolution_express/app.js
+++ section_005/assignmentSolution_express/app.js
@@ -1,20 +1,29 @@
 const express = require('express');
 
-const app = ecpress();
+const app = express();
+const bodyParser = require('body-parser');
+const epressHandlebars = require('express-handlebars');
+
 const port = 3000;
 
-app.set('view engine', 'pug');
+const users = [];
+
+app.engine('hbs', epressHandlebars.engine({defaultLayout: 'main-layout', extname: 'hbs'}));
+app.set('view engine', 'ejs'); // 'pug', 'ejs', 'hbs'
 app.set('views', 'views');
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', (req, res, next) => {
-    res.render();
+    res.render('index', { pageTitle: 'Add User' });
 });
 
 app.get('/users', (req, res, next) => {
-    res.render();
+    res.render('users', { pageTitle: 'Users', users: users, hasUsers: users.length > 0 });
 });
 
 app.post('/add-user', (req, res, next) => {
+    users.push({ name: req.body.username });
     res.redirect('/users');
 });
 
